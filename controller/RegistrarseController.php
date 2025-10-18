3eca9923c2fc0f94cf5aa5ffa130c6fb6773c978
JulianImperiale <julian.m.imperiale@gmail.com>
2025-10-18 14:43:44 -0300

    Agrega sweet alert al registro y el metodo apra subir foto

--- controller/RegistrarseController.php
+++ controller/RegistrarseController.php
@@ -22,12 +22,37 @@ class RegistrarseController
     }
     public function registrarse()
     {
-        $this->model->registrarUsuario($_POST["usuario"], $_POST["password"], $_POST["email"], $_POST["fechaNacimiento"], $_POST["foto_perfil"], $_POST["nombre_completo"]);
+        $usuario = trim($_POST["usuario"]);
+        $password = trim($_POST["password"]);
+        $email = trim($_POST["email"]);
+        $fecha = $_POST["fechaNacimiento"];
+        $nombre = trim($_POST["nombre_completo"]);
+        if (strlen($password) < 6 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "<script>
+                alert('Error: contraseña o email inválido');
+                window.history.back();
+            </script>";
+            return;
+        }
+        $foto_perfil = $this->subirFoto();
+        $this->model->registrarUsuario($usuario, $password, $email, $fecha, $nombre);
+
+
         $this->redirectToIndex();
     }
-        public function redirectToIndex()
+    public function redirectToIndex()
     {
         header("Location: /Preguntados/index.php?controller=Pokemon&method=base");
         exit;
+    }
+    private function subirFoto()
+    {
+        if (isset($_FILES['foto_perfil']) && $_FILES['foto_perfil']['error'] === UPLOAD_ERR_OK) {
+            $nombre = uniqid() . "_" . $_FILES['foto_perfil']['name'];
+            $destino = "uploads/" . $nombre;
+            move_uploaded_file($_FILES['foto_perfil']['tmp_name'], $destino);
+            return $destino;
+        }
+        return "uploads/default.png"; // imagen por defecto
     }
 }
